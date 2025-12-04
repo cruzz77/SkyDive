72a9e624a6599c8fa54ec0961343e0780001540c
Aditya Chopra <nytc120605@gmail.com>
2025-12-05 01:53:54 +0530

    Fixing issues again

--- frontend-user/vite.config.js
+++ frontend-user/vite.config.js
@@ -2,19 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://skydive-backend-qzsl.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
